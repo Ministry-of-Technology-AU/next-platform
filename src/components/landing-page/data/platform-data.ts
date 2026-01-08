922b567007116183cc71c85a8dae5e38350f59c6
Soham Tulsyan <soham.tulsyan_ug2023@ashoka.edu.in>
2026-01-08 20:44:30 +0530

    Added Neev Banner for carousel, reduced latency to 3.5 seconds, fixed alignment issues and responsiveness

--- src/components/landing-page/data/platform-data.ts
+++ src/components/landing-page/data/platform-data.ts
@@ -1,6 +1,6 @@
 
 import { BannerButton } from "./types";
-import { ClipboardPenLine, CalendarSync, Calendar, Award} from 'lucide-react';
+import { ClipboardPenLine, CalendarSync, Calendar, Award } from 'lucide-react';
 
 export const banners: Array<{
   id: number;
@@ -12,43 +12,60 @@ export const banners: Array<{
   gradient: string;
   buttons?: BannerButton[];
 }> = [
-        
-  {
-    id: 1,
-    title: "",
-    subtitle: "",
-    description: "",
-    image: "/z6ggcs3unvoquykoyupo.webp",
-    alt: "Welcome to the Platform!",
-    gradient: "",
-  },
-  {
-    id: 2,
-    title: "",
-    subtitle: "",
-    description: "",
-    image: "/xvs8kyobnxkrgzgeym5o.webp",
-    alt: "Meet our team!",
-    gradient: "",
-  },
-  {
-    id: 3,
-    title: "This space is for your advertisements!",
-    subtitle: "Dear Organizations:",
-    description: "If you want to advertise here, please reach out to us via mail at technology.ministry@ashoka.edu.in",
-    image: "/yyh4iiocug5dnctfkd5t.webp",
-    alt: "Default Advertisement Banner",
-    gradient: "",
-    buttons:[
+
+    {
+      id: 1,
+      title: "",
+      subtitle: "",
+      description: "",
+      image: "/z6ggcs3unvoquykoyupo.webp",
+      alt: "Welcome to the Platform!",
+      gradient: "",
+    },
+    {
+      id: 2,
+      title: "NEEV MEMBER INDUCTIONS OPEN!",
+      subtitle: "Spring 2025",
+      description: "Forms open till 12th Jan EOD",
+      image: "/Neev-Banner.webp",
+      alt: "Neev Member Inductions Open!",
+      gradient: "dark:bg-black/20",
+      buttons: [
         {
-            variant: "animatedGhost",
-            children: "Click here to contact Us",
-            onClick: () => window.location.href = "mailto:technology.ministry@ashoka.edu.in",
-            className: "hover:underline text-secondary",
+          variant: "default",
+          children: "Apply Now!",
+          onClick: () => window.location.href = "mailto:technology.ministry@ashoka.edu.in",
+          className: "bg-secondary-light text-green hover:bg-green-light/80 hover:text-secondary-light uppercase font-black",
         }
-    ]
-  },
-];
+      ]
+    },
+    {
+      id: 3,
+      title: "",
+      subtitle: "",
+      description: "",
+      image: "/xvs8kyobnxkrgzgeym5o.webp",
+      alt: "Meet our team!",
+      gradient: "",
+    },
+    {
+      id: 4,
+      title: "This space is for your advertisements!",
+      subtitle: "Dear Organizations:",
+      description: "If you want to advertise here, please reach out to us via mail at technology.ministry@ashoka.edu.in",
+      image: "/yyh4iiocug5dnctfkd5t.webp",
+      alt: "Default Advertisement Banner",
+      gradient: "",
+      buttons: [
+        {
+          variant: "animatedGhost",
+          children: "Click here to contact Us",
+          onClick: () => window.location.href = "mailto:technology.ministry@ashoka.edu.in",
+          className: "hover:underline text-secondary",
+        }
+      ]
+    },
+  ];
 
 export const recentlyVisited = [
   {
@@ -58,21 +75,21 @@ export const recentlyVisited = [
     href: "/platform/course-reviews",
     lastVisited: new Date('2024-10-05'),
   },
-    {
+  {
     id: 2,
     name: "Semester Planner",
     icon: CalendarSync,
     href: "/platform/semester-planner",
     lastVisited: new Date('2024-10-05'),
   },
-      {
+  {
     id: 3,
     name: "CGPA Planner",
     icon: Award,
     href: "/platform/cgpa-planner",
     lastVisited: new Date('2024-10-05'),
   },
-        {
+  {
     id: 4,
     name: "Events Calendar",
     icon: Calendar,
