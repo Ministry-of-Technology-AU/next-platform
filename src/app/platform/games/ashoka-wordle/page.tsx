6bb633b5b634270ce2c4b48fa1dbf8aac35f1ee9
Soham Tulsyan <soham.tulsyan_ug2023@ashoka.edu.in>
2026-01-21 23:51:54 +0530

    Wordle Added

---
+++ src/app/platform/games/ashoka-wordle/page.tsx
@@ -0,0 +1,7 @@
+export default function AshokaWordlePage() {
+    return (
+        <div>
+            <iframe height="700px" width="100%" allow="web-share; fullscreen" style={{ border: "none", width: "100% !important", position: "static", display: "block !important", margin: "0 !important", overflow: "hidden", backgroundColor: "transparent" }} src="https://puzzleme.amuselabs.com/pmm/date-picker?set=b667278412-ashoka-wordle&embed=1" aria-label="Puzzle Me Game"> </iframe>
+        </div>
+    );
+}
